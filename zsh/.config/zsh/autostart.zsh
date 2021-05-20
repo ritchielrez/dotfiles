a6241300e220a604762c931d92dbddfc287172ef
RitchielRez <ritchielrez@gmail.com>
2021-05-20 13:31:22 +0600

    Changed the zsh theme depending on the wgtk color scheme

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -1,5 +1,7 @@
 cat ~/.config/zsh/todo.txt
 source ~/.config/zsh/todo.zsh
 cd ~/Documents/dev/cpp/GTKTut2
-wpg -s /home/ritux/Documents/repos/wallpapers/maller/awake.jpg ; clear
+# wpg -s /home/ritux/Documents/repos/wallpapers/maller/awake.jpg ; clear
 
+# Set wgtk color scheme in zsh
+(cat ~/.cache/wal/sequences &)
