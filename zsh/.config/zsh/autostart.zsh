fbe1b7eb0b7a3daca7e6ff6695320a79f0ff66b5
RitchielRez <ritchielrez@gmail.com>
2021-06-12 15:54:03 +0600

    Updated autostart.zsh

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -1,7 +1,7 @@
 cat ~/.config/zsh/todo.txt
 source ~/.config/zsh/todo.zsh
 cd ~/Documents/dev/cpp/GTKTut2
-# wpg -s /home/ritux/Documents/repos/wallpapers/maller/awake.jpg ; clear
+wpg -s /home/ritux/Documents/repos/wallpapers/maller/awake.jpg ; clear
 
 # Set wgtk color scheme in zsh
-(cat ~/.cache/wal/sequences &)
+# (cat ~/.cache/wal/sequences &)
